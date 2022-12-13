41c5ba92f058f406608cb91836a58b920202c926
Roman <roman.kasparian@gmail.com>
2022-12-13 10:57:12 -0500

    clean up default cra src

---
+++ src/views/App/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    ></div>
+  );
+};
+
+export default App;
