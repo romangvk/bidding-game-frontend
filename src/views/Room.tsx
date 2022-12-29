3913503a73a415825c533075358197b21d941fba
Roman <roman.kasparian@gmail.com>
2022-12-29 12:16:53 -0500

    add room ui

--- src/views/Room.tsx
+++ src/views/Room.tsx
@@ -1,9 +1,70 @@
-import { useParams } from "react-router-dom";
+import { onValue, ref } from "firebase/database";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import View from "../components/View";
+import { database } from "../firebase/init";
+import { increment } from "../services/functions";
 
 const Room = () => {
   const { room } = useParams();
-  return <View>{room}</View>;
+  const [lobby, setLobby] = useState<{
+    players: { [uid: string]: { name: string; order: number } };
+  }>();
+  const [game, setGame] = useState<{ data: number }>();
+  console.log(lobby);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const query = ref(database, `lobbies/${room}`);
+    return onValue(
+      query,
+      (snapshot) => {
+        if (snapshot.exists()) {
+          setLobby(snapshot.val());
+        }
+      },
+      (error) => {
+        console.error(error.message);
+        navigate("/");
+      }
+    );
+  }, [room, navigate]);
+
+  useEffect(() => {
+    const query = ref(database, `games/${room}`);
+    return onValue(
+      query,
+      (snapshot) => {
+        if (snapshot.exists()) {
+          setGame(snapshot.val());
+        }
+      },
+      (error) => {
+        console.error(error.message);
+      }
+    );
+  }, [room]);
+
+  return (
+    <View>
+      <h1>{room}</h1>
+      <input
+        type="button"
+        value={game ? game.data : "Start Game"}
+        onClick={() => increment()}
+      />
+      <div style={{ display: "flex", gap: "1em", flexWrap: "wrap" }}>
+        {lobby
+          ? Object.entries(lobby.players).map(([uid, player], i) => (
+              <h3 key={uid}>
+                {player.name} {player.order}
+              </h3>
+            ))
+          : "Loading..."}
+      </div>
+    </View>
+  );
 };
 
 export default Room;
